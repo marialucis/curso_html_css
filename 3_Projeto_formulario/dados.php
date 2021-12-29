28a8935db02c832e1f6f15f6056b814bba336266
marialucis <marialuci148@gmail.com>
2021-12-29 01:25:09 -0300

    Novos arquivos atual

---
+++ 3_Projeto_formulario/dados.php
@@ -0,0 +1,19 @@
+<?php
+/*recendo os dados dos formularios, variaveis
+*/
+
+$nome = $_POST['nome'];
+$cpf = $_POST['cpf'];
+
+
+echo "Dados do Formulario <br>" ;
+
+echo "Nome completo: <br>" .$nome ;
+echo "CPF: " .$cpf;
+
+
+
+
+
+
+?>
